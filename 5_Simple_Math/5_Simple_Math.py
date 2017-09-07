4d200ca3bc0e9e8a8157ee1a8093ffd533a1e2db
gyjoo <gyjoo3748@gmail.com>
2017-09-07 17:20:24 +0900

    Problem 5 solved

---
+++ 5_Simple_Math/5_Simple_Math.py
@@ -0,0 +1,12 @@
+fnum = eval(input("What is the first number? "))
+snum = eval(input("What is the second number? "))
+
+plus = fnum + snum
+minus = fnum - snum
+multiply = fnum * snum
+divide = fnum / snum
+
+print(fnum, "+" , snum , "=", plus)
+print(fnum, "-" , snum , "=", minus)
+print(fnum, "*" , snum , "=", multiply)
+print(fnum, "/" , snum , "= %d"%divide)
