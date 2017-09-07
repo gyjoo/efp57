29fface08556f6be460e6112833f6c4c77d24505
gyjoo <gyjoo3748@gmail.com>
2017-09-07 17:23:42 +0900

    Problem 6 solved

---
+++ 6_Retirement_Calculator/6_Retirement_Calculator.py
@@ -0,0 +1,5 @@
+age = eval(input("What is your current age? "))
+retire_age = eval(input("At what age would you like to retire? "))
+
+print("You have", retire_age - age, "years left until you can retire.")
+print("It's 2017, so you can retire in", retire_age - age + 2017)
