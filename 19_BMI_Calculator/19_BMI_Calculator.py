0107a49307dc8409912434e4a73ebec4b1d6650f
gyjoo <gyjoo3748@gmail.com>
2017-09-07 23:17:10 +0900

    Problem 19 solved

---
+++ 19_BMI_Calculator/19_BMI_Calculator.py
@@ -0,0 +1,11 @@
+weight = eval(input("What is your weight? (kg) "))
+height = eval(input("How tall are you? (m) "))
+
+bmi = weight/(height*height) # Use kg and m
+
+print("Your BMI is %.1f." %bmi)
+
+if 18.5<bmi<25:
+	print("You are within the ideal weight range.")
+else:
+	print("You are overweight. You should see your doctor.")
