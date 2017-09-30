8b97353dd5fbec7d1850a97e0ede2612698795d9
주기영 <gyjoo@postech.ac.kr>
2017-09-30 22:54:24 +0900

    revise indentations

--- 18_Temperature_Converter/18_Temperature_Converter.py
+++ 18_Temperature_Converter/18_Temperature_Converter.py
@@ -5,10 +5,10 @@ choice = input("Your choice: ")
 temp = 0 # Set default value
 
 if choice in ["c", "C"]:
-	temp = eval(input("\nPlease enter the temperature in Fahrenheit: "))
-	print("The temperature in Celsius is %d." %((temp-32)*5/9))
-elif choice in ["F", "f"]:	
-	temp = eval(input("\nPlease enter the temperature in Celsius: "))
-	print("The temperature in Fahrenheit is %d." %((temp*9/5)+32))
+    temp = eval(input("\nPlease enter the temperature in Fahrenheit: "))
+    print("The temperature in Celsius is %d." %((temp - 32) * 5 / 9))
+elif choice in ["F", "f"]:
+    temp = eval(input("\nPlease enter the temperature in Celsius: "))
+    print("The temperature in Fahrenheit is %d." %((temp * 9 / 5) + 32))
 else:
-	print("You should type C or F. Please try again.")
+    print("You should type C or F. Please try again.")
